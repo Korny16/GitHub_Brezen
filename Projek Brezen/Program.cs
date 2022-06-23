965364760cf3f910ee0cd749f39d07cc2e3416ad
Korny <Korny@DESKTOP-VPU26AH>
2022-06-23 08:42:51 +0200

    Přidejte soubory projektu.

---
+++ Projek Brezen/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Zadej první číslo: ");
+int cislo1 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+Console.Write("Zadej druhé číslo: ");
+int cislo2 = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+if (cislo1 > cislo2)
+{
+    Console.WriteLine($"Číslo {cislo1} je větší než číslo {cislo2}");
+}
+if (cislo1 == cislo2)
+{
+    Console.WriteLine($"Čísla jsou stejně velká");
+}
+else
+{
+    Console.WriteLine($"Číslo {cislo2} je větší než číslo {cislo1}");
+}
